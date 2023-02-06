41091f50f7220abbae0410ccf861f1928e4fa869
chl <chl@aliquote.org>
2023-02-06 13:48:25 +0100

    [r] Formatting and logging

--- r/hand02.r
+++ r/hand02.r
@@ -1,137 +1,127 @@
-## ---- eval = FALSE-------------------------------------------------------
-## install.packages(c("ggplot2", "hrbrthemes", "directlabels", "cowplot",
-##                      "texreg", "rms", "mfp", "multcomp", "reshape2"))
-
-## ---- message = FALSE----------------------------------------------------
-library(hrbrthemes)
 library(directlabels)
 library(cowplot)
 library(texreg)
-library(rms)            ## Imports: Hmisc, ggplot2
+library(rms) ## Imports: Hmisc, ggplot2
 library(mfp)
 library(multcomp)
 library(reshape2)
 options(digits = 6, show.signif.stars = FALSE)
-theme_set(theme_ipsum(base_size = 11))
+theme_set(theme_minimal(base_size = 11))
 
-## ------------------------------------------------------------------------
+con <- file("hand02.log")
+sink(con, append = TRUE)
+
 load("../data/fev.rda")
 str(FEV)
 
-## ------------------------------------------------------------------------
 summary(FEV)
 
-## ------------------------------------------------------------------------
 describe(FEV$age)
-FEV$age[FEV$age == 3] <- 4         ## low counts in extreme categories
+FEV$age[FEV$age == 3] <- 4 ## low counts in extreme categories
 FEV$age[FEV$age == 19] <- 18
-FEV$height <- FEV$height * 2.54  ## inches -> centimeters
+FEV$height <- FEV$height * 2.54 ## inches -> centimeters
 FEV$lfev <- log(FEV$fev)
 
-## ---- warning = FALSE----------------------------------------------------
 ## switch to long format in order to get a facet variable
-d <- melt(FEV[,c("id", "age", "fev", "height")], id.vars = "id")
+d <- melt(FEV[, c("id", "age", "fev", "height")], id.vars = "id")
 levels(d$variable) <- c("Age (yr.)", "FEV (l/s)", "Height (cm)")
 p <- ggplot(data = d, aes(x = value)) +
-       geom_line(stat = "density") +
-       geom_rug(size = .5, alpha = .3) +
-       facet_wrap(~ variable, ncol = 3, scales = "free")  +
-       labs(x = "", y = "Density")
-p
+  geom_line(stat = "density") +
+  geom_rug(size = .5, alpha = .3) +
+  facet_wrap(~variable, ncol = 3, scales = "free") +
+  labs(x = "", y = "Density")
+ggsave("figs/fig-02-01.pdf", width = 8, height = 6)
 
-## ---- warning = FALSE----------------------------------------------------
 ## switch to long format in order to get a facet variable
-d <- melt(FEV[,c("id", "sex", "smoke", "age", "fev")], measure.vars = 4:5)
+d <- melt(FEV[, c("id", "sex", "smoke", "age", "fev")], measure.vars = 4:5)
 levels(d$variable) <- c("Age (yr.)", "FEV (l/s)")
 p <- ggplot(data = d, aes(x = sex, y = value)) +
-       geom_boxplot() +
-       facet_grid(variable ~ smoke, scales = "free_y") +
-       labs(x = NULL, y = NULL)
-p
+  geom_boxplot() +
+  facet_grid(variable ~ smoke, scales = "free_y") +
+  labs(x = NULL, y = NULL)
+ggsave("figs/fig-02-02.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 p <- ggplot(data = FEV, aes(x = age, y = fev, color = sex)) +
-       geom_point(alpha = 0.5) +
-       geom_smooth(method = "loess", se = FALSE) +
-       scale_color_manual(values = c("steelblue", "orange")) +
-       guides(color = FALSE) +
-       labs(x = "Age (yr.)", y = "FEV (l/s)")
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "loess", se = FALSE) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  guides(color = FALSE) +
+  labs(x = "Age (yr.)", y = "FEV (l/s)")
 p1 <- direct.label(p + aes(label = sex))
 p <- ggplot(data = FEV, aes(x = height, y = fev, color = sex)) +
-       geom_point(alpha = 0.5) +
-       geom_smooth(method = "loess", se = FALSE) +
-       scale_color_manual(values = c("steelblue", "orange")) +
-       guides(color = FALSE) +
-       labs(x = "Height (cm)", y = "FEV (l/s)")
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "loess", se = FALSE) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  guides(color = FALSE) +
+  labs(x = "Height (cm)", y = "FEV (l/s)")
 p2 <- direct.label(p + aes(label = sex))
-plot_grid(p1, p2)
 
-## ------------------------------------------------------------------------
+cairo_pdf("figs/fig-02-03.pdf", width = 8, height = 6)
+plot_grid(p1, p2)
+dev.off()
+
 m <- lm(fev ~ height, data = FEV)
 summary(m)
 
-## ------------------------------------------------------------------------
 yhat <- fitted(m)
 rstd <- rstandard(m)
 p <- ggplot(data = NULL, aes(x = yhat, y = rstd)) +
-       geom_hline(yintercept = 0, linetype = 1, color = grey(.3)) +
-       geom_hline(yintercept = c(-2,2), linetype = 2, color = grey(.3)) +
-       geom_point() +
-       geom_smooth(method = "loess", se = FALSE, color = "lightcoral") +
-       labs(x = "Fitted values", y = "Residuals")
-p
+  geom_hline(yintercept = 0, linetype = 1, color = grey(.3)) +
+  geom_hline(yintercept = c(-2, 2), linetype = 2, color = grey(.3)) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE, color = "lightcoral") +
+  labs(x = "Fitted values", y = "Residuals")
+ggsave("figs/fig-02-04.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
-m <- lm(I(fev ^ (1/3)) ~ height, data = FEV)
+m <- lm(I(fev^(1 / 3)) ~ height, data = FEV)
 
-## ---- echo = FALSE-------------------------------------------------------
 yhat <- fitted(m)
 names(yhat) <- FEV$id
 rstd <- rstandard(m)
 p <- ggplot(data = NULL, aes(x = yhat, y = rstd)) +
-       geom_hline(yintercept = 0, linetype = 1, color = grey(.3)) +
-       geom_hline(yintercept = c(-2,2), linetype = 2, color = grey(.3)) +
-       geom_point() +
-       annotate("text", x = yhat[abs(rstd) > 3], y = rstd[abs(rstd) > 3],
-                label = names(yhat[abs(rstd) > 3]), size = 3, hjust = -0.5) +
-       geom_smooth(method = "loess", se = FALSE, color = "lightcoral") +
-       labs(x = "Fitted values", y = "Residuals", caption = ~ "Model considering height" ^3)
-p
+  geom_hline(yintercept = 0, linetype = 1, color = grey(.3)) +
+  geom_hline(yintercept = c(-2, 2), linetype = 2, color = grey(.3)) +
+  geom_point() +
+  annotate("text",
+    x = yhat[abs(rstd) > 3], y = rstd[abs(rstd) > 3],
+    label = names(yhat[abs(rstd) > 3]), size = 3, hjust = -0.5
+  ) +
+  geom_smooth(method = "loess", se = FALSE, color = "lightcoral") +
+  labs(x = "Fitted values", y = "Residuals", caption = ~ "Model considering height"^3)
+ggsave("figs/fig-02-05.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 influence.measures(m)
 
-## ------------------------------------------------------------------------
 m1 <- lm(fev ~ height + I(height^2), data = FEV)
-m2 <- lm(fev ~ pol(height, 3), data = FEV)        ## or poly(height, 3, raw = TRUE)
+m2 <- lm(fev ~ pol(height, 3), data = FEV) ## or poly(height, 3, raw = TRUE)
 m3 <- lm(fev ~ rcs(height, 3), data = FEV)
 
-## ------------------------------------------------------------------------
 m4 <- mfp(fev ~ fp(height, df = 4, select = .05), data = FEV)
 m4
 
-## ---- echo = FALSE-------------------------------------------------------
 dd <- data.frame(height = 110:190)
-yhat <- cbind.data.frame(dd, y1 = predict(m1, dd), y2 = predict(m2, dd),
-                         y3 = predict(m3, dd), y4 = predict(m4, dd))
+yhat <- cbind.data.frame(dd,
+  y1 = predict(m1, dd), y2 = predict(m2, dd),
+  y3 = predict(m3, dd), y4 = predict(m4, dd)
+)
 p <- ggplot(data = FEV, aes(x = height, y = fev)) +
-       geom_point(color = grey(.3), alpha = .5) +
-       geom_line(data = melt(yhat, measure.vars = 2:5), aes(x = height, y = value, color = variable), size = 1) +
-       scale_color_ipsum(name = "", labels = c("height + height²", "pol(height, 3)", "rcs(height, 3)", "fp(height, 4)")) +
-       theme(legend.position = c(0.1, 0.88)) +
-       labs(x = "Height (cm)", y = "FEV (l/s)")
-p
+  geom_point(color = grey(.3), alpha = .5) +
+  geom_line(data = melt(yhat, measure.vars = 2:5), aes(x = height, y = value, color = variable), size = 1) +
+  scale_color_manual("",
+    values = c("#d18975", "#8fd175", "#3f2d54", "#75b8d1"),
+    labels = c("height + height²", "pol(height, 3)", "rcs(height, 3)", "fp(height, 4)")
+  ) +
+  theme(legend.position = c(0.1, 0.88)) +
+  labs(x = "Height (cm)", y = "FEV (l/s)")
+ggsave("figs/fig-02-06.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 levels(FEV$smoke)
 m <- lm(fev ~ smoke, data = FEV)
 summary(m)
 confint(m)
 
-## ------------------------------------------------------------------------
 t.test(fev ~ smoke, data = FEV, var.equal = TRUE)
 
-## ------------------------------------------------------------------------
 FEV$age4 <- cut2(FEV$age, g = 4)
 r <- with(FEV, summarize(fev, age4, smean.sd))
 r
@@ -140,114 +130,112 @@ m1 <- lm(fev ~ age, data = FEV)
 m2 <- lm(fev ~ age4, data = FEV)
 m3 <- aov(fev ~ age4, data = FEV)
 
-## ------------------------------------------------------------------------
 summary(m2)
 
-## ------------------------------------------------------------------------
 r <- with(FEV, summarize(fev, age4, smean.sd))
 r
 
-## ---- echo = FALSE-------------------------------------------------------
 FEV$age4c <- as.numeric(as.character(cut2(FEV$age, g = 4, levels.mean = TRUE)))
 p <- ggplot(data = FEV, aes(x = age, y = fev)) +
-       geom_boxplot(aes(x = age4c, y = fev, group = age4c), outlier.size = -1, color = grey(.3), fill = "transparent") +
-       geom_jitter(color = grey(.7), alpha = .5, width = .05) +
-       geom_smooth(method = "lm", color = "steelblue") +
-       labs(x = "Age (yr.)", y = "FEV (l/s)")
-p
+  geom_boxplot(aes(x = age4c, y = fev, group = age4c), outlier.size = -1, color = grey(.3), fill = "transparent") +
+  geom_jitter(color = grey(.7), alpha = .5, width = .05) +
+  geom_smooth(method = "lm", color = "steelblue") +
+  labs(x = "Age (yr.)", y = "FEV (l/s)")
+ggsave("figs/fig-02-07.pdf", width = 8, height = 6)
 FEV$age4c <- NULL
 
-## ---- echo = FALSE-------------------------------------------------------
 p <- ggplot(data = FEV, aes(x = age, y = fev, color = smoke)) +
-       geom_point(alpha = 0.5) +
-       geom_smooth(method = "loess", se = FALSE) +
-       annotate("rect", xmin = -Inf, ymin = -Inf, xmax = 9, ymax = Inf, fill = grey(.7), alpha = .5) +
-       scale_color_manual(values = c("steelblue", "orange")) +
-       guides(color = FALSE) +
-       labs(x = "Age (yr.)", y = "FEV (l/s)")
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "loess", se = FALSE) +
+  annotate("rect", xmin = -Inf, ymin = -Inf, xmax = 9, ymax = Inf, fill = grey(.7), alpha = .5) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  guides(color = FALSE) +
+  labs(x = "Age (yr.)", y = "FEV (l/s)")
+cairo_pdf("figs/fig-02-08.pdf", width = 8, height = 6)
 direct.label(p + aes(label = smoke))
+dev.off()
 
-## ------------------------------------------------------------------------
 m1 <- lm(fev ~ smoke, data = FEV)
 m2 <- lm(fev ~ age + smoke, data = FEV)
-screenreg(list(m1,m2))
+screenreg(list(m1, m2))
 
-## ------------------------------------------------------------------------
 anova(m1, m2)
 
-## ------------------------------------------------------------------------
 m3 <- lm(fev ~ age + smoke + age:smoke, data = FEV)
 summary(m3)
 
-## ------------------------------------------------------------------------
 p <- ggplot(data = FEV, aes(x = age, y = fev, color = smoke)) +
-       geom_point(alpha = 0.5) +
-       geom_smooth(method = "lm", show.legend = FALSE) +
-       scale_color_manual("", values = c("steelblue", "orange")) +
-       guides(color = FALSE) +
-       labs(x = "Age (yr.)", y = "FEV (l/s)")
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", show.legend = FALSE) +
+  scale_color_manual("", values = c("steelblue", "orange")) +
+  guides(color = FALSE) +
+  labs(x = "Age (yr.)", y = "FEV (l/s)")
+cairo_pdf("figs/fig-02-09.pdf", width = 8, height = 6)
 direct.label(p + aes(label = smoke))
+dev.off()
 
-## ------------------------------------------------------------------------
+
 m <- ols(fev ~ age + smoke + age:smoke + pol(height, 2), data = FEV, x = TRUE)
 m
 
-## ------------------------------------------------------------------------
-d <- expand.grid(age = 9:18, smoke = levels(FEV$smoke),
-                 height = seq(120, 180, by = 10))
+d <- expand.grid(
+  age = 9:18, smoke = levels(FEV$smoke),
+  height = seq(120, 180, by = 10)
+)
 yhat <- predict(m, d, se.fit = TRUE)
 d <- cbind.data.frame(d, yhat)
 head(d)
 
-## ------------------------------------------------------------------------
-Predict(m, age = 18, height = seq(160, 180, by = 2),
-        smoke = "current smoker", conf.type = "simult")
+Predict(m,
+  age = 18, height = seq(160, 180, by = 2),
+  smoke = "current smoker", conf.type = "simult"
+)
 
-## ------------------------------------------------------------------------
 data(anorexia, package = "MASS")
 str(anorexia)
 
-## ------------------------------------------------------------------------
-anorexia$Prewt <- anorexia$Prewt * 0.45    ## lbs -> kg
+anorexia$Prewt <- anorexia$Prewt * 0.45 ## lbs -> kg
 anorexia$Postwt <- anorexia$Postwt * 0.45
 anorexia$Treat <- relevel(anorexia$Treat, ref = "Cont")
 describe(anorexia)
 
-## ------------------------------------------------------------------------
 p <- ggplot(data = anorexia, aes(x = Prewt, y = Postwt, color = Treat)) +
-       geom_point() +
-       geom_smooth(method = "lm", se = FALSE) +
-       scale_color_manual("", values = c("grey30", "steelblue", "orange")) +
-       guides(color = FALSE) +
-       labs(x = "Weight at entry (kg)", y = "Weight at discharge (kg)")
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  scale_color_manual("", values = c("grey30", "steelblue", "orange")) +
+  guides(color = FALSE) +
+  labs(x = "Weight at entry (kg)", y = "Weight at discharge (kg)")
+cairo_pdf("figs/fig-02-10.pdf", width = 8, height = 6)
 direct.label(p + aes(label = Treat))
+dev.off()
 
-## ------------------------------------------------------------------------
 m0 <- lm(Postwt ~ Prewt + Treat, data = anorexia, subset = Treat != "Cont")
 m1 <- lm(Postwt ~ Prewt + Treat + Prewt:Treat, data = anorexia, subset = Treat != "Cont")
 anova(m0, m1)
 
-## ------------------------------------------------------------------------
 summary(m0)
 confint(m0)
 
-## ------------------------------------------------------------------------
 data(birthwt, package = "MASS")
 summary(birthwt)
 
-## ------------------------------------------------------------------------
 birthwt$lwt <- birthwt$lwt * 0.45
 birthwt$race <- factor(birthwt$race, levels = 1:3, labels = c("white", "black", "other"))
 birthwt$smoke <- factor(birthwt$smoke, levels = 0:1, labels = c("non-smoker", "smoker"))
 p <- ggplot(data = birthwt, aes(x = lwt, y = bwt, color = smoke)) +
-       geom_point() +
-       geom_smooth(method = "lm", se = FALSE) +
-       scale_color_manual(values = c("steelblue", "orange")) +
-       guides(color = FALSE) +
-       facet_wrap(~ race, ncol = 3) +
-       geom_text(data = data.frame(lwt = c(45,90), bwt = c(1500,4500),
-                                   race = "white", smoke = c("smoker", "non-smoker")),
-                 aes(x = lwt, y = bwt, label = smoke), color = c("orange", "steelblue")) +
-       labs(x = "Mother weight (kg)", y = "Baby weight (g)")
-p
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  guides(color = FALSE) +
+  facet_wrap(~race, ncol = 3) +
+  geom_text(
+    data = data.frame(
+      lwt = c(45, 90), bwt = c(1500, 4500),
+      race = "white", smoke = c("smoker", "non-smoker")
+    ),
+    aes(x = lwt, y = bwt, label = smoke), color = c("orange", "steelblue")
+  ) +
+  labs(x = "Mother weight (kg)", y = "Baby weight (g)")
+ggsave("figs/fig-02-11.pdf", width = 8, height = 6)
 
+sink()
--- r/hand04.r
+++ r/hand04.r
@@ -1,11 +1,5 @@
-## ---- eval = FALSE-------------------------------------------------------
-## install.packages(c("ggplot2", "ggfortify", "hrbrthemes", "directlabels",
-##                      "cowplot", "texreg", "lme4", "geepack")
-
-## ---- message = FALSE----------------------------------------------------
 library(ggplot2)
 library(ggfortify)
-library(hrbrthemes)
 library(directlabels)
 library(cowplot)
 library(texreg)
@@ -15,61 +9,54 @@ library(lme4)
 library(geepack)
 library(reshape2)
 options(digits = 6, show.signif.stars = FALSE)
-theme_set(theme_ipsum(base_size = 11))
+theme_set(theme_minimal(base_size = 11))
 
-## ------------------------------------------------------------------------
+con <- file("hand04.log")
+sink(con, append = TRUE)
+
 load("../data/fat.rda")
 str(fat)
 
-## ------------------------------------------------------------------------
 summary(fecfat ~ pilltype, data = fat, fun = smean.sd)
 
-## ------------------------------------------------------------------------
 d <- aggregate(fecfat ~ pilltype, data = fat, mean)
 p <- ggplot(data = fat, aes(x = reorder(pilltype, fecfat), y = fecfat)) +
        geom_line(aes(group = subject), color = grey(.3)) +
        geom_line(data = d, aes(x = pilltype, y = fecfat, group = 1),
                  color = "lightcoral", size = 1.2) +
        labs(x = NULL, y = "Fecal Fat")
-p
+ggsave("figs/fig-04-01.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 m1 <- aov(fecfat ~ pilltype, data = fat)
 m2 <- aov(fecfat ~ pilltype + subject, data = fat)
 m3 <- aov(fecfat ~ pilltype + Error(subject), data = fat)
 summary(m3)
 
-## ------------------------------------------------------------------------
 m <- lmer(fecfat ~ pilltype + (1 | subject), data = fat)
 summary(m)
 confint(m)
 
-## ------------------------------------------------------------------------
 yhat <- predict(m)
 p <- ggplot(data = fat, aes(x = reorder(pilltype, fecfat), y = yhat)) +
        geom_line(aes(group = subject), color = grey(.3)) +
        geom_line(data = d, aes(x = pilltype, y = fecfat, group = 1),
                  color = "lightcoral", size = 1.2) +
        labs(x = NULL, y = "Predicted Fecal Fat")
-p
+ggsave("figs/fig-04-02.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 sepsis <- foreign::read.dta("../data/sepsis.dta")
 str(sepsis)
 
-## ------------------------------------------------------------------------
 sepsis$treat <- as.factor(sepsis$treat)
 sepsis$id <- factor(sepsis$id)
 sepsis.long <- melt(sepsis, measure.vars = 8:14, id.vars = 1:2)
 celsius2fahr <- function(x) (x-32) / 1.8              ## °F -> °C
 sepsis.long$value <- celsius2fahr(sepsis.long$value)
 
-## ------------------------------------------------------------------------
 s <- summary(treat ~ fate + race + apache + temp0 + temp1, data = sepsis,
              method = "reverse", overall = TRUE)
 print(s, digits = 3)
 
-## ------------------------------------------------------------------------
 p <- ggplot(data = na.omit(sepsis.long), aes(x = variable, y = value)) +
        geom_line(aes(group = id), col = grey(.7), alpha = .2) +
        geom_line(data = subset(na.omit(sepsis.long), variable %in% c("temp0", "temp1")),
@@ -79,9 +66,8 @@ p <- ggplot(data = na.omit(sepsis.long), aes(x = variable, y = value)) +
        scale_color_manual("", values = c("steelblue", "orange")) +
        labs(x = "Durée de suivi (heure)", y = "Température (°C)") +
        theme(legend.position = c(.5, 1.05), legend.direction = "horizontal")
-p
+ggsave("figs/fig-04-03.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 d <- with(sepsis.long, summarize(value, llist(treat, variable), smean.cl.normal))
 
 p <- ggplot(data = d, aes(x = variable, y = value, shape = treat, color = treat)) +
@@ -93,9 +79,8 @@ p <- ggplot(data = d, aes(x = variable, y = value, shape = treat, color = treat)
        guides(color = FALSE, shape = FALSE) +
        geom_dl(aes(label = treat), method = list("smart.grid", cex = .8)) +
        labs(x = "Durée de suivi (heure)", y = "Température (°C)")
-p
+ggsave("figs/fig-04-04.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 ## 0 = alive, 1 = dead
 st <- with(sepsis, Surv(time = followup, event = as.numeric(fate)-1))
 s <- survfit(st ~ treat, data = sepsis)
@@ -105,9 +90,8 @@ p <- autoplot(s, censor = FALSE) +
        guides(fill = FALSE) +
        theme(legend.position = c(.9, .9)) +
        labs(x = "Followup time (hr.)", y = "Probability survival")
-p
+ggsave("figs/fig-04-05.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 cc <- aggregate(value ~ treat + id, sepsis.long, length)
 idx <- cc$id[cc$value == 7]  ## complete case IDs
 d <- subset(sepsis.long, id %in% idx)
@@ -116,29 +100,23 @@ names(d)[3] <- "time"
 levels(d$time) <- 0:6
 d$time <- as.numeric(as.character(d$time))
 
-## ------------------------------------------------------------------------
 ## lme(value ~ time * treat, data = d, random = ~ 1 | id)
 m1 <- lmer(value ~ time * treat + (1 | id), data = d)
 summary(m1)
 
-## ------------------------------------------------------------------------
 ## lme(value ~ time * treat, data = d, random = ~ time | id)
 m2 <- lmer(value ~ time * treat + (time | id), data = d)
 
-## ------------------------------------------------------------------------
 ## lme(value ~ time * treat, data = d, random = list(id = pdDiag(~ time)))
 m3 <- lmer(value ~ time * treat + (1 | id) + (0 + time | id), data = d)
 
 ## lme(value ~ time * treat, data = d, random = ~ 0 + time | id)
 m4 <- lmer(value ~ time * treat + (0 + time | id), data = d)
 
-## ------------------------------------------------------------------------
 screenreg(list(m1, m2, m3), include.variance = TRUE)
 
-## ------------------------------------------------------------------------
 anova(m1, m3, m2)
 
-## ---- echo = FALSE-------------------------------------------------------
 d$yhat1 <- predict(m1)
 d$yhat2 <- predict(m2)
 p1 <- ggplot(data = d, aes(x = time, y = yhat1, color = treat)) +
@@ -157,18 +135,17 @@ p2 <- ggplot(data = d, aes(x = time, y = yhat2, color = treat)) +
        guides(color = FALSE) +
        geom_dl(aes(label = treat), method = "smart.grid") +
        labs(x = "Durée de suivi (heure)", y = "Température (°C)", caption = "Model 2")
+cairo_pdf("figs/fig-04-06.pdf", width = 8, height = 6)
 plot_grid(p1, p2)
+dev.off()
 
-## ------------------------------------------------------------------------
 m <- geeglm(value ~ time * treat, data = d, id = id, corstr = "exch", scale.fix=TRUE)
 summary(m)
 
-## ------------------------------------------------------------------------
 m0 <- geese(value ~ time * treat, data = d, id = id, corstr = "exch", scale.fix=TRUE)
 m0$vbeta
 m0$vbeta.naiv
 
-## ------------------------------------------------------------------------
 d$yhat3 <- predict(m)
 p <- ggplot(data = d, aes(x = time, y = yhat3, color = treat)) +
        geom_line(aes(group = id), alpha = .2) +
@@ -178,13 +155,12 @@ p <- ggplot(data = d, aes(x = time, y = yhat3, color = treat)) +
        guides(color = FALSE) +
        geom_dl(aes(label = treat), method = "smart.grid") +
        labs(x = "Durée de suivi (heure)", y = "Température (°C)", caption = "Model GEE")
-p
+ggsave("figs/fig-04-07.pdf", width = 8, height = 6)
 
-## ------------------------------------------------------------------------
 data(ohio, package = "geepack")
 str(ohio)
 ohio$age <- ohio$age + 9    ## in years
 
-## ------------------------------------------------------------------------
 summary(resp ~ age + smoke, data = ohio, method = "cross")
 
+sink()
