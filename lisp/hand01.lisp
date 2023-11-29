fc47038e2728f3a4925fc7de345a39b281d47d2b
chl <chl@aliquote.org>
2023-11-29 07:27:43 +0100

    [lisp] Use plain text instead of csv

--- lisp/hand01.lisp
+++ lisp/hand01.lisp
@@ -1,6 +1,9 @@
 (ql:quickload "cl-ana")
 (in-package :cl-ana)
 
-(defparameter *data* (open-csv-table "/home/chl/cwd/biostats/data/polymorphism.csv"))
+(defparameter *data* (open-csv-table "/home/chl/cwd/biostats/data/polymorphism.dat" 
+				     :delimeter #\SPACE 
+				     :field-names '("age" "genotype")))
 (table-field-names *data*)
+(table-summarize *data*)
 
